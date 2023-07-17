c234f3aae922d2bcdb8dc6167ee7f00c3913bbb5
akif <akifhasdemir@hotmail.com>
2023-07-18 02:18:40 +0300

    Initial commit

---
+++ src/nLayeredDemo/core/LoggerService.java
@@ -0,0 +1,9 @@
+package nLayeredDemo.core;
+
+public interface LoggerService {
+    // Başkalarının sistemlerini, hazır kodları onlara bağlı olmadan kendi projemize eklemek için yazdığımız interfacedir.
+
+    void logToSystem(String message);
+}
+
+
